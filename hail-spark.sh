e828815d5648f7e4337bf3247713cbab46b4cbd6
Indraniel Das <idas@genome.wustl.edu>
2017-01-18 16:23:12 -0600

    + add the needed c/c++ dependencies for hail

--- hail-spark.sh
+++ hail-spark.sh
@@ -2,6 +2,16 @@
 
 ROOT=/gscmnt/gc2802/halllab/idas
 
+# needed because of the c/c++ code embedded in hail
+GCC_BIN=/opt/gcc-4.8.4/bin
+GCC_LIBS=/opt/gcc-4.8.4/lib64
+OPENBLAS_LIBS=/gscmnt/gc2802/halllab/idas/software/local/lib
+CMAKE_BIN=/gscuser/idas/software/cmake/bin
+
+export PATH=${CMAKE_BIN}:${GCC_BIN}:${PATH}
+export LD_LIBRARY_PATH=${GCC_LIBS}:${OPENBLAS_LIBS}:${LD_LIBRARY_PATH}
+
+export PYSPARK_PYTHON=/gscuser/idas/.pyenv/versions/2.7.10/bin/python
 export SPARK_HOME=${ROOT}/software/local/spark-2.0.2-bin-hadoop2.7
 export LSF_SPARK_WRAPPER=${ROOT}/hail-play/lsf-spark-wrapper.sh
 export LOCAL_HAIL=${ROOT}/software/downloads/github/hail/build/install/hail/bin/hail
